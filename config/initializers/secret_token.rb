df78839391ca189d6d0c3f572caf65448fffd444
Ozzie Gooen <Ozzie@BowLabs.org>
2013-04-18 16:11:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feedhavenorg::Application.config.secret_token = 'a4575d0daca851625dabcd1ec9b4998227afa37753da0457a07d544a3157b71f01875347b36f89d8ff40997a89df6c96a0265175273b57748a31a46d962a087a'
